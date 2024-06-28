64e968dc18737e6318071795d06de44ce9d9541b
Vtencouchclimbr <148500829+Vtencouchclimbr@users.noreply.github.com>
2024-06-28 17:42:08 -0600

    add formsubmission function

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,8 +1,25 @@
 // TODO: Create a variable that selects the form element
 let formEl = document.getElementsByTagName('form');
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the redirectPage function. If the form is submitted with missing data, display an error message to the user.
-let userInput = function(event) {
+let formSubmission = function(event) {
+  event.preventDefault();
+
+  const username = document.getElementById('username').value;
+  const title = document.getElementById('title').value;
+  const content = document.getElementById('content').value;
+
+  if(!username || !title || !content) {
+    alert("Please complete the form");
+    return;
+  }
   
+  const formData = {
+    username: username,
+    title: title,
+    content: content
+  };
+localStorage.setItem('formData', JSON.stringify(formData));
+
 }
 
 let redirectURL = '';
